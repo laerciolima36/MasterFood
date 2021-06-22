8fa2d3345e1bd74f334db7d5997deca824bae8e1
laercio.lima <laercio.lima@NB_PE058R09.qger.coi>
2021-06-22 08:52:47 -0300

    Primeira Compilação GitHub

---
+++ MasterFood/src/main/java/com/teslasolucoes/AppController.java
@@ -0,0 +1,62 @@
+package com.teslasolucoes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@GetMapping("/index")
+	public String viewHomePage() {
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
+	}
+	
+	@GetMapping("/pedidoMesa")
+	public String showpedidoMesa() {
+		return "pedidoMesa";
+	}
+	
+	@GetMapping("/pedidoDelivery")
+	public String showpedidoDelivery() {
+		return "pedidoDelivery";
+	}
+	
+	@GetMapping("/pedidoCozinha")
+	public String showpedidoCozinha() {
+		return "pedidoCozinha";
+	}
+	
+	@GetMapping("/list_users")
+	public String showUsuarios(Model model) {
+		List<User> listUsers = repo.findAll();
+		model.addAttribute("listUsers", listUsers);
+		return "users";
+	}
+	
+	
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String encodedPassword = encoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		
+		repo.save(user);
+		return "register_sucess";
+	}
+}
---
+++ MasterFood/src/test/java/com/teslasolucoes/MasterFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teslasolucoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
